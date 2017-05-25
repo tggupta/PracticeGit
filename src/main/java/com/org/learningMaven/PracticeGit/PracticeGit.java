daac485ca777ccfc0fbc95e5545aa709dc915f97
tggupta <tggupta22@gmail.com>
2017-05-25 23:11:40 +0530

    77

--- src/main/java/com/org/learningMaven/PracticeGit/PracticeGit.java
+++ src/main/java/com/org/learningMaven/PracticeGit/PracticeGit.java
@@ -4,7 +4,7 @@ public class PracticeGit {
 
 	public static void main(String[] args) {
 		System.out.println("Practice Git and Githubs");
-		System.out.println("pdddra5445445i easzcsammmce1122");
+		System.out.println("pddd777ra5445445i easzcsammmce1122");
 
 	}
 
